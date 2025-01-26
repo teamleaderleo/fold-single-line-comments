dbd9e79b0aa5b4ac33ee787f5332797f12df7108
Leo Li <leoli_082000@hotmail.com>
2025-01-25 21:39:46 -0500

    add test files for ruby (i think it doesn't support folding # comments?) and .sh and .yaml files

---
+++ examples/test.rb
@@ -0,0 +1,7 @@
+# test.rb
+# Ruby
+def test
+    # Method comments
+    # Should fold
+    puts "test"
+end
---
+++ examples/test.sh
@@ -0,0 +1,6 @@
+# test.sh
+# Shell scripts common
+# In DevOps/automation
+echo "test" # inline comment
+# More comments
+# To fold
